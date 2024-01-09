190d00c98d8d4a2374a07f847944f763d6163d95
acrisanto <alanvcrisanto@gmail.com>
2024-01-08 21:08:38 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { NewTodoForm } from "./components/NewTodoForm";
+import { TodoList } from "./components/TodoList";
+import "./App.css";
+
+function App() {
+	const [todos, setTodos] = useState(() => {
+		const localValue = localStorage.getItem("Item");
+		if (localValue == null) return [];
+
+		return JSON.parse(localValue);
+	});
+
+	useEffect(() => {
+		localStorage.setItem("Item", JSON.stringify(todos));
+	}, [todos]);
+
+	function addTodo(title) {
+		setTodos((currentTodos) => {
+			return [...currentTodos, { id: crypto.randomUUID(), title, completed: false }];
+		});
+	}
+
+	function toggleTodo(id, completed) {
+		setTodos((currentTodos) => {
+			return currentTodos.map((todo) => {
+				if (todo.id === id) {
+					return { ...todo, completed };
+				}
+				return todo;
+			});
+		});
+	}
+
+	function deleteTodo(id) {
+		setTodos((currentTodos) => {
+			return currentTodos.filter((todo) => todo.id !== id);
+		});
+	}
+
+	return (
+		<>
+			<NewTodoForm addTodo={addTodo} />
+			<h1 className="header">Todo List</h1>
+			<TodoList todos={todos} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />
+		</>
+	);
+}
+
+export default App;
